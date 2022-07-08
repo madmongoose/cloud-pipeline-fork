1a09df48e239d5f916de51b97906604cf9da0159
Roman Gorokhovskii <roman_gorokhovskii@epam.com>
2022-07-08 14:37:33 +0300

    # - name: Install Dependencies  # - name: Build GRADLE

--- deploy/gha/gha-pack-dist.sh
+++ deploy/gha/gha-pack-dist.sh
@@ -54,11 +54,13 @@ _BUILD_DOCKER_IMAGE="lifescience/cloud-pipeline:python2.7-centos6" ./gradlew -Pb
 mv pipe-cli/dist/dist-file/pipe ${API_STATIC_PATH}/pipe-el6
 mv pipe-cli/dist/dist-folder/pipe.tar.gz ${API_STATIC_PATH}/pipe-el6.tar.gz
 
-# ./gradlew distTar   -PbuildNumber=$GITHUB_RUN_NUMBER.$GITHUB_SHA \
-#                     -Pprofile=release \
-#                     -x test \
-#                     -Pfast \
-#                     --no-daemon
+export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+
+./gradlew distTar   -PbuildNumber=$GITHUB_RUN_NUMBER.$GITHUB_SHA \
+                    -Pprofile=release \
+                    -x test \
+                    -Pfast \
+                    --no-daemon
 
 #removed sudo
 #sudo mv pipe-cli/dist/dist-file/pipe ${API_STATIC_PATH}/pipe-el6
