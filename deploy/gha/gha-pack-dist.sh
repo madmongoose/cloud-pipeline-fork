f24fb5ce637250e2f3151f747954a3a8d48bf50d
Roman Gorokhovskii <roman_gorokhovskii@epam.com>
2022-07-08 12:31:12 +0300

    # pip3 install wheel

--- deploy/gha/gha-pack-dist.sh
+++ deploy/gha/gha-pack-dist.sh
@@ -16,10 +16,10 @@
 
 set -e
 
-sudo apt-get install gcc libpq-dev -y
-sudo apt-get install python-dev  python-pip -y
-sudo apt-get install python3-dev python3-pip python3-venv python3-wheel -y
-pip3 install wheel
+# sudo apt-get install gcc libpq-dev -y
+# sudo apt-get install python-dev  python-pip -y
+# sudo apt-get install python3-dev python3-pip python3-venv python3-wheel -y
+# pip3 install wheel
 
 # echo `python -V`
 # echo `pip -V`
