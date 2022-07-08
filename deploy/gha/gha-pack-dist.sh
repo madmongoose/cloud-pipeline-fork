da46f5c37be92ae621ae07397cf391a24e696850
Roman Gorokhovskii <roman_gorokhovskii@epam.com>
2022-07-08 12:44:21 +0300

    _BUILD_DOCKER_IMAGE=

--- deploy/gha/gha-pack-dist.sh
+++ deploy/gha/gha-pack-dist.sh
@@ -47,8 +47,11 @@ tar -zxf $_OSX_CLI_PATH/$_OSX_CLI_TAR_NAME -C $_OSX_CLI_PATH
 sudo mv $_OSX_CLI_PATH/dist/dist-file/pipe-osx ${API_STATIC_PATH}/pipe-osx
 sudo mv $_OSX_CLI_PATH/dist/dist-folder/pipe-osx.tar.gz ${API_STATIC_PATH}/pipe-osx.tar.gz
 
-_BUILD_DOCKER_IMAGE="${CP_DOCKER_DIST_SRV}lifescience/cloud-pipeline:python2.7-centos6" 
-sudo ./gradlew -PbuildNumber=$GITHUB_RUN_NUMBER.$GITHUB_SHA --info -Pprofile=release pipe-cli:buildLinux --no-daemon -x :pipe-cli:test
+#_BUILD_DOCKER_IMAGE="${CP_DOCKER_DIST_SRV}lifescience/cloud-pipeline:python2.7-centos6" 
+#sudo ./gradlew -PbuildNumber=$GITHUB_RUN_NUMBER.$GITHUB_SHA --info -Pprofile=release pipe-cli:buildLinux --no-daemon -x :pipe-cli:test
+_BUILD_DOCKER_IMAGE="${CP_DOCKER_DIST_SRV}lifescience/cloud-pipeline:python2.7-centos6" ./gradlew -PbuildNumber=$GITHUB_RUN_NUMBER.$GITHUB_SHA -Pprofile=release pipe-cli:buildLinux --no-daemon -x :pipe-cli:test
+
+
 #removed sudo
 sudo mv pipe-cli/dist/dist-file/pipe ${API_STATIC_PATH}/pipe-el6
 sudo mv pipe-cli/dist/dist-folder/pipe.tar.gz ${API_STATIC_PATH}/pipe-el6.tar.gz
