b3b51e1d9dcd3c3c9b9886c456438a78c167c807
Roman Gorokhovskii <roman_gorokhovskii@epam.com>
2022-07-11 14:02:05 +0300

    #set -e

--- deploy/gha/gha-pack-dist.sh
+++ deploy/gha/gha-pack-dist.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
+#set -e
 
 # sudo apt-get install gcc libpq-dev -y
 # sudo apt-get install python-dev  python-pip -y
